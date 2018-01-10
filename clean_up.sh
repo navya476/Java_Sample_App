8a699ead706baf6a6a3233572337be2e02370c95
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-10 19:11:29 +0530

    Update clean_up.sh

--- clean_up.sh
+++ clean_up.sh
@@ -1,4 +1,12 @@
 Container_status=`docker ps -a | grep "jboss_container"`
+Image_status=`docker ps -a | grep "jboss_application"`
+if [ ! -z "$Image_status" ];
+then
+     docker rmi jboss_application -f
+else
+     echo "There is no container named jboss_container"
+fi
+
 if [ ! -z "$Container_status" ];
 then
      docker rm jboss_container -f
