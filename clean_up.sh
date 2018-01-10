84d267e3d9e369d70dead36cbb1952c6e3a5160a
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-10 18:47:07 +0530

    Update clean_up.sh

--- clean_up.sh
+++ clean_up.sh
@@ -1,7 +1,6 @@
-echo "In Clean_up.sh"
 echo `docker ps -a | grep "jboss_container"`
-if [[ ! -z `docker ps -a | grep "jboss_container"` ]]; then
-docker rm -f jboss_container
+if [[ ! -z `docker ps -a | grep "jboss_container"` ]]; then 
+     docker rm -f jboss_container
 else 
  echo "There is no container named jboss_container"
 fi
