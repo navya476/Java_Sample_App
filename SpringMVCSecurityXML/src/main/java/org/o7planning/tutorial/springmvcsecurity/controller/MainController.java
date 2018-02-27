19c96792bb17bcf2ae27ac3e2ef546df56cf0c94
Sunil Boga <30490515+boga5@users.noreply.github.com>
2018-02-27 18:24:41 +0530

    Update MainController.java

--- SpringMVCSecurityXML/src/main/java/org/o7planning/tutorial/springmvcsecurity/controller/MainController.java
+++ SpringMVCSecurityXML/src/main/java/org/o7planning/tutorial/springmvcsecurity/controller/MainController.java
@@ -12,8 +12,8 @@ public class MainController {
  
     @RequestMapping(value = { "/", "/welcome" }, method = RequestMethod.GET)
     public String welcomePage(Model model) {
-        model.addAttribute("title", "Welcome");
-        model.addAttribute("message", "This is welcome page!");
+        model.addAttribute("title", "Helloo People!! \n");
+        model.addAttribute("message", "The Aplication is working perfect!");
         return "welcomePage";
     }
  
